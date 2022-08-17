949680a36cc27c2cb64f077b840cf198b8ca8bab
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-08-17 13:59:33 +0530

    changin layout to Layout in [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import Head from "next/head";
